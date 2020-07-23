206b974ff50b405759c4391ae05a14bb47853a4d
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-07-23 21:15:08 +0200

    Create MovieDetailPresenter.swift

---
+++ MovieZoo/Modules/Movie Details/Presenter/MovieDetailPresenter.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+class MovieDetailPresenter: MovieDetailPresenterProtocol {
+    weak var view: MovieDetailViewProtocol?
+    var interactor: MovieDetailInteractorProtocol?
+    var router: MovieDetailRouterProtocol?
+    
+    private var movieDetailViewModel: MovieDetailViewModel?
+    private var movie: Movie
+    
+    init(movie: Movie) {
+        self.movie = movie
+    }
+    
+    func viewDidLoad() {
+        loadMovieDetail()
+    }
+    
+    func viewWillAppear() {}
+    
+    
+    private func loadMovieDetail() {
+        view?.showLoading(message: "Loading...")
+        interactor?.makeMovieDetailRequest(movieTitle: movie.title)
+    }
+    
+    func viewAllCast() {
+        router?.pushMovieCastScene(view: view!, movie: movie)
+    }
+}
+
+extension MovieDetailPresenter: MovieDetailInteractorOutputProtocol {
+    func onMovieDetailSuccess(moviePhotos: [FlickrPhoto]) {
+        view?.hideLoading()
+        movieDetailViewModel = MovieDetailViewModel(movieDetail: movie, moviePhotos: moviePhotos)
+        view?.showMovieDetail(viewModel: movieDetailViewModel!)
+    }
+    
+    func onMovieDetailError(error: APIError) {
+        view?.hideLoading()
+        view?.showErrorView(type: .Custom(title: nil, desc: error.description, image: Image.icEmptyState.image, buttonAction: "Retry"))
+    }
+}
