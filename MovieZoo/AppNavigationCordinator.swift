71a6f8eab68ad7ddc316e0b7c49e12638a64bd14
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-07-23 21:17:52 +0200

    Create AppNavigationCordinator.swift

---
+++ MovieZoo/AppNavigationCordinator.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class AppNavigationCordinator {
+    static func buildMovieListModule() -> UIViewController {
+        let viewController = UIStoryboard(name: "MovieList", bundle: nil)
+            .instantiateViewController(withIdentifier: "MovieListViewController") as! MovieListViewController
+        
+        let presenter: MovieListPresenterProtocol & MovieListInteractorOutputProtocol = MovieListPresenter()
+        let interactor: MovieListInteractorProtocol = MovieListInteractor(manager: LocalDataManager<[Movie]>())
+        let router: MovieListRouterProtocol = MovieListRouter()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        return viewController
+    }
+    
+    static func buildMovieDetailModule(movie: Movie) -> UIViewController {
+        let viewController
+            = UIStoryboard(name: "MovieDetail", bundle: nil)
+            .instantiateViewController(withIdentifier: "MovieDetailViewController") as! MovieDetailViewController
+
+        let presenter: MovieDetailPresenterProtocol & MovieDetailInteractorOutputProtocol = MovieDetailPresenter(movie: movie)
+        let interactor: MovieDetailInteractorProtocol = MovieDetailInteractor()
+        let router: MovieDetailRouterProtocol = MovieDetailRouter()
+
+        viewController.presenter = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        return viewController
+    }
+
+    static func buildSearchMoviesModule() -> UIViewController {
+        let viewController = MovieSearchResultViewController()
+        
+        let presenter: MovieSearchPresenterProtocol & MovieSearchInteractorOutputProtocol = MovieSearchPresenter()
+        let interactor: MovieSearchInteractorProtocol = MovieSearchInteractor(dataManager: LocalDataManager<[Movie]>())
+        let router: MovieSearchRouterProtocol = MovieSearchRouter()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        return viewController
+    }
+}
