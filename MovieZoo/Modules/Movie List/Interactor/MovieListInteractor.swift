46bc9ea8fe359e359d849845ca3bbc3bca407b29
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-07-23 21:15:37 +0200

    Create MovieListInteractor.swift

---
+++ MovieZoo/Modules/Movie List/Interactor/MovieListInteractor.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class MovieListInteractor: MovieListInteractorProtocol {
+    weak var presenter: MovieListInteractorOutputProtocol?
+    private var dataManager: LocalDataManager<[Movie]>?
+        
+    init(manager: LocalDataManager<[Movie]>) {
+        self.dataManager = manager
+    }
+    
+    func makeMovieListRequest() {
+        if let movies = dataManager?.loadData(from: "movies") {
+            self.presenter?.onMoviesFetchSuccess(movies: movies)
+        } else {
+            self.presenter?.onMoviesFetchError(error: APIError.emptyData)
+        }
+    }
+}
