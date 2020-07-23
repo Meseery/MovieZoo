b760fee4d41fef5c50d8a8e9c004ffb86f9d7df4
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-07-23 21:16:03 +0200

    Create MovieSearchContracts.swift

---
+++ MovieZoo/Modules/Movie Search/Contracts/MovieSearchContracts.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+typealias MovieSearchResult = (year:Int, movies: [Movie])
+
+protocol MovieSearchRouterProtocol: class {
+    func dismiss(view: MovieSearchViewProtocol)
+    func pushMovieDetailScene(from view: UIViewController, movie: Movie)
+}
+
+protocol MovieSearchViewProtocol: BaseView {
+    var presenter: MovieSearchPresenterProtocol? { get set }
+    
+    func showSearchResults(searchResults: [MovieSearchResult])
+}
+
+protocol MovieSearchPresenterProtocol: BasePresenter {
+    var view: MovieSearchViewProtocol? { get set }
+    var interactor: MovieSearchInteractorProtocol? { get set }
+    var router: MovieSearchRouterProtocol? { get set }
+    
+    func searchResultsCount() -> Int
+    func searchResultsPerSection(section: Int) -> Int
+    func didSearchMovie(searchText: String)
+    func didSelectMovie(at indexPath: IndexPath)
+    func movieSearchResult(at indexPath: IndexPath) -> MovieSearchResult?
+    func movie(at indexPath: IndexPath) -> Movie?
+}
+
+protocol MovieSearchInteractorProtocol: class {
+    var presenter: MovieSearchInteractorOutputProtocol? { get set }
+    
+    func searchMovie(searchText: String)
+    func movieSearchResult(at indexPath: IndexPath) -> MovieSearchResult?
+    func allMovieSearchResults() -> [MovieSearchResult]
+}
+
+protocol MovieSearchInteractorOutputProtocol: class {
+    func onMovieSearchSuccess(searchResults: [MovieSearchResult])
+    func onMovieSearchFailure(error: APIError)
+}
